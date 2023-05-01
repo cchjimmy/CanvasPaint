1e91d6b66ffe7e77648d2cf8bea6e082bda67ae4
cchjimmy <92587458+cchjimmy@users.noreply.github.com>
2023-05-01 16:48:57 +0100

    optimization: no redrawing history every frame

--- src/main.js
+++ src/main.js
@@ -3,59 +3,79 @@
   const ctx = canvas.getContext("2d");
   const history = [];
   const path = [];
-  const pos = { x: 0, y: 0 };
   const keys = new Map();
   const removedPaths = [];
   const commandQueue = [];
   var pointerDown = false;
+  const config = {
+    background: "black",
+    color: "white",
+    ctx: {
+      lineWidth: 5,
+      lineCap: "round",
+      lineJoin: "round"
+    }
+  }
   main();
   function main() {
     document.body.appendChild(canvas);
     resizeCanvas(innerWidth, innerHeight);
-    ctx.lineWidth = 1;
-    ctx.lineCap = "round";
+    ctx.strokeStyle = config.color;
+    background();
     update();
   }
   function update() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    ctx.strokeStyle = "white";
-
-    for (let i = 0; i < history.length; i++) {
-      strokePath(history[i]);
-    }
-
-    strokePath(path);
-
     executeCommands();
     requestAnimationFrame(update);
+  }
+  function background() {
+    ctx.fillStyle = config.background;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
   function resizeCanvas(w, h) {
     canvas.width = w;
     canvas.height = h;
+    for (prop in config.ctx) {
+      ctx[prop] = config.ctx[prop];
+      console.log(ctx[prop]);
+    }
   }
   function strokePath(path) {
-    if (!path || !path.length) return;
+    if (!path) return;
     ctx.beginPath();
 
-    ctx.moveTo(path[0].x, path[0].y);
-    for (let i = 0; i < path.length; i++) {
-      ctx.lineTo(path[i].x, path[i].y);
+    ctx.moveTo(path[0], path[1]);
+    for (let i = 2; i < path.length; i += 2) {
+      ctx.lineTo(path[i], path[i + 1]);
     }
 
     ctx.stroke();
   }
+  function line(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+  }
+  function drawHistory() {
+    background();
+    for (let i = 0; i < history.length; i++) {
+      strokePath(history[i]);
+    }
+  }
   window.onresize = () => resizeCanvas(innerWidth, innerHeight);
   window.onpointermove = (e) => {
-    pos.x = e.clientX;
-    pos.y = e.clientY;
-    if (pointerDown) path.push({ x: pos.x, y: pos.y });
+    if (pointerDown) {
+      if (path.length) {
+        line(path[path.length - 2], path[path.length - 1], e.clientX, e.clientY);
+      }
+      path.push(e.clientX, e.clientY)
+    };
   };
   window.onpointerdown = () => pointerDown = true;
   window.onpointerup = () => {
     if (path.length) {
-      history.push(path.slice(0));
+      history.push(path.slice());
       path.splice(0);
     }
     pointerDown = false;
@@ -91,12 +111,15 @@
   function undo() {
     let path = history.pop();
     if (path) removedPaths.push(path);
+    drawHistory();
   }
   function redo() {
     let path = removedPaths.pop();
     if (path) history.push(path);
+    drawHistory();
   }
   function clear() {
+    background();
     history.splice(0);
     removedPaths.splice(0);
     path.splice(0);
